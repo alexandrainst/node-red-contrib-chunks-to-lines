75e2dd08028c0db9ca43b287cc16cf31133e14c6
Alexandre Alapetite <alexandre.alapetite@alexandra.dk>
2023-06-01 17:31:27 +0200

    Better test

--- test.sh
+++ test.sh
@@ -2,7 +2,8 @@
 
 test=$(
 	cat <<'EOF' | node ./index.js chunks-to-lines --linesFormat='"csv"' --nbLines='2' | sed 's/"id":"[0-9.]*"/"id":""/g'
-{"payload": "A;B;C\n1;2;3\n4;5;6\n7;8;9\n10;11;12\n", "complete": true}
+{"payload": "A;B;C\n1;2;3\n"}
+{"payload": "4;5;6\n7;8;9\n10;11;12\n", "complete": true}
 {"tick": true}
 {"tick": true}
 {"payload": "D;E;F\n21;22;23\n24;25;26\n27;28;29\n30;31;32\n", "complete": true}
